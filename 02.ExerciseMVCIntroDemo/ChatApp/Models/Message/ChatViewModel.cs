4063e6f4a128187823318d9fe8815989a03d4094
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2023-09-02 19:48:58 +0300

    Add files via upload

---
+++ 02.ExerciseMVCIntroDemo/ChatApp/Models/Message/ChatViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace ChatApp.Models.Message
+{
+    using Microsoft.AspNetCore.Authentication.Cookies;
+
+    public class ChatViewModel
+    {
+        public ChatViewModel()
+        {
+            this.Messages = new HashSet<MessageViewModel>();
+        }
+
+        public MessageViewModel CurrentMessage { get; set; } = null!;
+
+        public ICollection<MessageViewModel> Messages { get; set; }
+    }
+}
