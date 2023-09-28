a76eb85ae3032f85ca124da1ba843b1d03b3bec6
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2023-09-28 23:50:49 +0300

    Add files via upload

---
+++ 03.WorkshopForumApp/Forum.Common/Validations/DataConstants.cs
@@ -0,0 +1,14 @@
+﻿namespace Forum.Common.Validations
+{
+    public static class DataConstants
+    {
+        public static class Post
+        {
+            public const int TitleMinL = 10;
+            public const int TitleMaxL = 50;
+
+            public const int ContentMinL = 30;
+            public const int ContentMaxL = 1500;
+        }
+    }
+}
