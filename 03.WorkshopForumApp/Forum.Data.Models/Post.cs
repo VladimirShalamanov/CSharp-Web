dd364ee20dae78836dd1d8a0747059e1db714f18
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2023-09-29 22:07:46 +0300

    Add files via upload

---
+++ 03.WorkshopForumApp/Forum.Data.Models/Post.cs
@@ -0,0 +1,25 @@
+﻿namespace Forum.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using static Forum.Common.Validations.DataConstants.Post;
+
+    public class Post
+    {
+        public Post()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(TitleMaxL)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [MaxLength(ContentMaxL)]
+        public string Content { get; set; } = null!;
+    }
+}
