498f7176bd2b28c91ee08c52b2bbed1c3a8bb66c
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2023-09-29 22:08:57 +0300

    Add files via upload

---
+++ 03.WorkshopForumApp/Forum.Services/Interfaces/IPostService.cs
@@ -0,0 +1,17 @@
+﻿namespace Forum.Services.Interfaces
+{
+    using Forum.ViewModels.Post;
+
+    public  interface IPostService
+    {
+        Task<IEnumerable<PostViewModel>> ListAllAsync();
+
+        Task AddPostAsync(PostAddFormModel postModel);
+        
+        Task<PostAddFormModel> GetForEditOrDeleteByIdAsync(string id);
+
+        Task EditByIdAsync(string id, PostAddFormModel postEditedModel);
+
+        Task DeleteByIdAsync(string id);
+    }
+}
---
+++ 03.WorkshopForumApp/Forum.Services/PostService.cs
@@ -0,0 +1,82 @@
+﻿namespace Forum.Services
+{
+    using Microsoft.EntityFrameworkCore;
+
+    using Forum.Data;
+    using Forum.ViewModels.Post;
+    using Forum.Services.Interfaces;
+    using Forum.Data.Models;
+    
+    public class PostService : IPostService
+    {
+        private readonly ForumAppDbContext dbContext;
+        
+        public PostService(ForumAppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        
+
+        public async Task<IEnumerable<PostViewModel>> ListAllAsync()
+        {
+            IEnumerable<PostViewModel> allPosts = await dbContext
+                .Posts
+                .Select(p => new PostViewModel()
+                {
+                    Id = p.Id.ToString(),
+                    Title = p.Title,
+                    Content = p.Content
+                })
+                .ToArrayAsync();
+
+            return allPosts;
+        }
+
+        public async Task AddPostAsync(PostAddFormModel postModel)
+        {
+            Post newPost = new Post()
+            {
+                Title = postModel.Title,
+                Content = postModel.Content
+            };
+            
+            await this.dbContext.Posts.AddAsync(newPost);
+            await this.dbContext.SaveChangesAsync();
+        }
+
+        public async Task<PostAddFormModel> GetForEditOrDeleteByIdAsync(string id)
+        {
+            Post postToEdit = await this.dbContext
+                .Posts
+                .FirstAsync(p => p.Id.ToString() == id);
+
+            return new PostAddFormModel()
+            {
+                Title = postToEdit.Title,
+                Content = postToEdit.Content
+            };
+        }
+
+        public async Task EditByIdAsync(string id, PostAddFormModel postEditedModel)
+        {
+            Post postToEdit = await this.dbContext
+                .Posts
+                .FirstAsync(p => p.Id.ToString() == id);
+
+            postToEdit.Title = postEditedModel.Title;
+            postToEdit.Content = postEditedModel.Content;
+
+            await this.dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteByIdAsync(string id)
+        {
+            Post postToDelete = await this.dbContext
+                .Posts
+                .FirstAsync(p => p.Id.ToString() == id);
+            
+            this.dbContext.Posts.Remove(postToDelete);
+            await this.dbContext.SaveChangesAsync();
+        }
+    }
+}
