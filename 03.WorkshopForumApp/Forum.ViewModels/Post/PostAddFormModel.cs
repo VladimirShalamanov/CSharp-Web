e1670340ecb9130d926f306b86177a0d840efc58
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2023-09-29 22:09:40 +0300

    Add files via upload

---
+++ 03.WorkshopForumApp/Forum.ViewModels/Post/PostAddFormModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Forum.ViewModels.Post
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using static Forum.Common.Validations.DataConstants.Post;
+
+    public class PostAddFormModel
+    {
+        [Required]
+        [StringLength(TitleMaxL, MinimumLength = TitleMinL)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [StringLength(ContentMaxL, MinimumLength = ContentMinL)]
+        public string Content { get; set; } = null!;
+    }
+}
---
+++ 03.WorkshopForumApp/Forum.ViewModels/Post/PostViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Forum.ViewModels.Post
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class PostViewModel
+    {
+        public string Id { get; set; } = null!;
+
+        public string Title { get; set; } = null!;
+
+        public string Content { get; set; } = null!;
+    }
+}
