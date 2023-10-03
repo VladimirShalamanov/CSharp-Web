3771359fc732cd21d54a395c334fb5206af09512
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2023-10-03 21:04:50 +0300

    Add files via upload

---
+++ 04.WatchlistApp - Prep/WatchlistApp_Skeleton/Watchlist/Models/AddMovieViewModel.cs
@@ -0,0 +1,36 @@
+﻿namespace Watchlist.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+    using Watchlist.Data.Models;
+
+    public class AddMovieViewModel
+    {
+        public AddMovieViewModel()
+        {
+            this.Genres = new HashSet<Genre>();    
+        }
+
+        [Required]
+        [StringLength(50, MinimumLength = 10)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [StringLength(50, MinimumLength = 5)]
+        public string Director { get; set; } = null!;
+
+        [Required(AllowEmptyStrings = false)]
+        public string ImageUrl { get; set; } = null!;
+
+        [Required]
+        [Range(typeof(decimal), "0.00", "10.00",
+            ConvertValueInInvariantCulture = true)]
+        public decimal Rating { get; set; }
+
+        public int GenreId { get; set; }
+
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
---
+++ 04.WatchlistApp - Prep/WatchlistApp_Skeleton/Watchlist/Models/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Watchlist.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class LoginViewModel
+    {
+        [Required]
+        public string UserName { get; set; } = null!;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+    }
+}
