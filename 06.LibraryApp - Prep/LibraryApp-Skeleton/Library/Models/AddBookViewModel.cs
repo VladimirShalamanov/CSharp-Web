9bd363a91f2988aca3f7c9fb881a1a09f971c150
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2023-10-11 21:07:36 +0300

    Add files via upload

---
+++ 06.LibraryApp - Prep/LibraryApp-Skeleton/Library/Models/AddBookViewModel.cs
@@ -0,0 +1,38 @@
+﻿namespace Library.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using static Data.DataConstants.Book;
+
+    public class AddBookViewModel
+    {
+        public AddBookViewModel()
+        {
+            this.Categories = new HashSet<CategoryViewModel>();
+        }
+
+        [Required]
+        [StringLength(BookTitle_MaxL, MinimumLength = BookTitle_MinL)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [StringLength(BookAuthor_MaxL, MinimumLength = BookAuthor_MinL)]
+        public string Author { get; set; } = null!;
+
+        [Required]
+        [StringLength(BookDescription_MaxL, MinimumLength = BookDescription_MinL)]
+        public string Description { get; set; } = null!;
+
+        [Required(AllowEmptyStrings = false)]
+        public string Url { get; set; } = null!;
+
+        [Required]
+        [Range(BookRating_MinL, BookRating_MaxL)]
+        public decimal Rating { get; set; }
+
+        [Range(1, int.MaxValue)]
+        public int CategoryId { get; set; }
+
+        public IEnumerable<CategoryViewModel> Categories { get; set; }
+    }
+}
---
+++ 06.LibraryApp - Prep/LibraryApp-Skeleton/Library/Models/BookViewModel.cs
@@ -0,0 +1,33 @@
+﻿namespace Library.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using static Library.Data.DataConstants.Book;
+
+    public class BookViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(BookTitle_MaxL, MinimumLength = BookTitle_MinL)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [StringLength(BookAuthor_MaxL, MinimumLength = BookAuthor_MinL)]
+        public string Author { get; set; } = null!;
+
+        [Required]
+        [StringLength(BookDescription_MaxL, MinimumLength = BookDescription_MinL)]
+        public string Description { get; set; } = null!;
+
+        [Required(AllowEmptyStrings = false)]
+        public string ImageUrl { get; set; } = null!;
+
+        [Required]
+        [Range(BookRating_MinL, BookRating_MaxL)]
+        public decimal Rating { get; set; }
+
+        [Range(1, int.MaxValue)]
+        public int CategoryId { get; set; }
+    }
+}
