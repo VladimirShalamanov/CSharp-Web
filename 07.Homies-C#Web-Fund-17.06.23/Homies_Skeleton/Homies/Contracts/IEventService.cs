587b37041380aa39d4966bb68d56d50bd3e66dd2
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2023-10-14 00:07:56 +0300

    Add files via upload

---
+++ 07.Homies-C#Web-Fund-17.06.23/Homies_Skeleton/Homies/Contracts/IEventService.cs
@@ -0,0 +1,29 @@
+﻿namespace Homies.Contracts
+{
+    using Homies.Models;
+
+    public interface IEventService
+    {
+        Task<IEnumerable<EventViewModel>> GetAllEventsAsync();
+        Task<IEnumerable<EventViewModel>> GetJoinedEventsAsync(string userId);
+
+
+        Task<IEnumerable<TypesViewModel>> GetAllTypesAsync();
+        Task AddEventAsync(EventFormViewModel model, string userId);
+
+
+        Task<EventFormViewModel?> GetEventForEditAsync(int id);
+        Task EditEventAsync(EventFormViewModel model, int id);
+
+
+        Task<EventDetailsViewModel> GetEventDetailsAsync(int id);
+
+        Task JoinEventAsync(int eventId, string userId);
+        Task LeaveEventAsync(int eventId, string userId);
+
+
+        Task<bool> IsOrganizerAsync(int eventId, string userId);
+        Task<bool> EventAlreadyJoinedAsync(int eventId, string userId);
+        Task<bool> EventExistsAsync(int id);
+    }
+}
