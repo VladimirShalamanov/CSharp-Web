8478e07f452f900e3801c89417398450d30b9bc4
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2023-10-14 00:09:13 +0300

    Add files via upload

---
+++ 07.Homies-C#Web-Fund-17.06.23/Homies_Skeleton/Homies/Models/EventFormViewModel.cs
@@ -0,0 +1,32 @@
+﻿namespace Homies.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using static Data.DataConstants.Event;
+
+    public class EventFormViewModel
+    {
+        public EventFormViewModel()
+        {
+            this.Types = new HashSet<TypesViewModel>();
+        }
+
+        [Required]
+        [StringLength(EventName_MaxL, MinimumLength = EventName_MinL)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [StringLength(EventDescription_MaxL, MinimumLength = EventDescription_MinL)]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        public DateTime Start { get; set; }
+
+        [Required]
+        public DateTime End { get; set; }
+
+        [Range(1, int.MaxValue)]
+        public int TypeId { get; set; }
+        public IEnumerable<TypesViewModel> Types { get; set; }
+    }
+}
