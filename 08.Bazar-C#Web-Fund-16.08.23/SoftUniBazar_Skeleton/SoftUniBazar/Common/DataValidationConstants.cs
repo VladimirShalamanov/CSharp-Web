8010567a072613db874059b4a537652fdda0d3d0
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2023-10-17 22:43:30 +0300

    Add files via upload

---
+++ 08.Bazar-C#Web-Fund-16.08.23/SoftUniBazar_Skeleton/SoftUniBazar/Common/DataValidationConstants.cs
@@ -0,0 +1,20 @@
+﻿namespace SoftUniBazar.Common
+{
+    public class DataValidationConstants
+    {
+        public static class Ad
+        {
+            public const int Name_MinLength = 5;
+            public const int Name_MaxLength = 25;
+
+            public const int Description_MinLength = 15;
+            public const int Description_MaxLength = 250;
+        }
+
+        public static class Category
+        {
+            public const int Name_MinLength = 3;
+            public const int Name_MaxLength = 15;
+        }
+    }
+}
