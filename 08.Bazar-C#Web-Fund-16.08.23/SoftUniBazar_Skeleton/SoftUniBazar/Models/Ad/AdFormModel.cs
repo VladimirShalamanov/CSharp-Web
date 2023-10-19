2322fea6928f5bc5aa54b0118ae061b16d75dba3
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2023-10-19 20:35:45 +0300

    Add files via upload

---
+++ 08.Bazar-C#Web-Fund-16.08.23/SoftUniBazar_Skeleton/SoftUniBazar/Models/Ad/AdFormModel.cs
@@ -0,0 +1,34 @@
+﻿namespace SoftUniBazar.Models.Ad
+{
+    using System.ComponentModel.DataAnnotations;
+    using Category;
+    using Data.Models;
+
+    using static Common.DataValidationConstants.Ad;
+
+    public class AdFormModel
+    {
+        public AdFormModel()
+        {
+            this.Categories = new HashSet<CategoryViewModel>();
+        }
+
+        [Required]
+        [StringLength(Name_MaxLength, MinimumLength = Name_MinLength)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [StringLength(Description_MaxLength, MinimumLength = Description_MinLength)]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        public string ImageUrl { get; set; } = null!;
+
+        public decimal Price { get; set; }
+
+        [Range(1, 5)]
+        public int CategoryId { get; set; }
+
+        public IEnumerable<CategoryViewModel> Categories { get; set; }
+    }
+}
